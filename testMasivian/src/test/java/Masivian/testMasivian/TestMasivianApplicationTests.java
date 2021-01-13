d201c2bd6f64f867ccdf0d16c99d899cbf59edda
Manuel V <manuelvictmont@gmail.com>
2021-01-13 17:19:02 -0500

    Start project

---
+++ testMasivian/src/test/java/Masivian/testMasivian/TestMasivianApplicationTests.java
@@ -0,0 +1,13 @@
+package Masivian.testMasivian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestMasivianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
