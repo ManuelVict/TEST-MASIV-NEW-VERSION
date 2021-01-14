d6091f1f1861f54e19d8967fe8447f9a0c98622c
Manuel V <manuelvictmont@gmail.com>
2021-01-14 17:55:57 -0500

    Define models
    Make two models, bet and roulette

---
+++ testMasivian/src/main/java/Masivian/testMasivian/model/Roulette.java
@@ -0,0 +1,37 @@
+package Masivian.testMasivian.model;
+import java.util.ArrayList;
+public class Roulette {
+    public long id;
+    public int number;
+    public boolean state;
+    public ArrayList bets = new ArrayList();
+
+    public long getId() {
+
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public int getNumber() {
+
+        return number;
+    }
+    public void setNumber(int number) {
+        this.number = number;
+    }
+    public boolean isState() {
+
+        return state;
+    }
+    public void setState(boolean state) {
+        this.state = state;
+    }
+    public ArrayList getBets() {
+
+        return bets;
+    }
+    public void setBets(ArrayList bets) {
+        this.bets = bets;
+    }
+}
